9e5109d6dceb9b98519f4dd657aa54127a96249d
aisharina99 <anis.aisharinaa@gmail.com>
2021-10-05 17:46:17 +0800

    Chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets, only: [:new, :create, :edit, :update]
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
 end
